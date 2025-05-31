836edb367f06e79682bf006e475ef3b8f9125ae6
GitHub Action <action@github.com>
2025-05-31 06:20:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/teddyyeung/checkunusedassetplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/teddyyeung/checkunusedassetplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.teddyyeung.checkunusedassetplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.teddyyeung.checkunusedassetplugin.MyBundle
+import com.github.teddyyeung.checkunusedassetplugin.services.MyProjectService
 import javax.swing.JButton
 
 
